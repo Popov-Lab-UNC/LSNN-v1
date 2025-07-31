020643fc8aefc36066c44a30d63cf348c1785c28
Rishabh Dey <rishabh1dey@gmail.com>
2025-07-31 17:58:27 -0400

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -1,5 +1,6 @@
 import subprocess
 
 def smi_to_protonated_sdf(smiles, output_path):
+    ''' Creates a protonated SDF file from the provided SMILES. '''
     cmd = f'obabel "-:{smiles}" -O "{output_path}" --gen3d --pH 7'
     subprocess.run(cmd, check=True, shell=True, timeout=60)
